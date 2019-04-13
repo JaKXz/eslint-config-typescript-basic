99f054a702024e9da4c40a571970489b62be66ae
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-13 08:04:57 +0200

    Replace deprecated peer dependencies with up-to-date packages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   overrides: [
     {
       files: '**/*.ts',
-      parser: 'typescript-eslint-parser',
+      parser: '@typescript-eslint/parser',
       parserOptions: {
         typescript: true
       },
